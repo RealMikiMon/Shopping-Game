57539c1f7ffd800a4db5d2bdcba8b54fa42df93f
RealMikiMon <monereomiquel@gmail.com>
2026-02-25 17:09:42 +0100

    HEalth UI

---
+++ Assets/Scripts/InventorySystem/UIElements/PlayerHealthUI.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerHealthUI : MonoBehaviour
+{
+    public int Health = 100;
+    public TextMeshProUGUI HealthText;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+    public void RestoreHealth(int amount)
+    {
+        Health += amount;
+        UpdateUI();
+    }
+
+    public void LoseHealth(int amount)
+    {
+        Health -= amount;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        HealthText.text = "Health" + Health.ToString();
+    }
+}
